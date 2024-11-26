67312dc1ae646a9e127851ab1c6a526d78a615a7
lmiel <lucia.mielgo@gmail.com>
2024-11-26 23:44:39 +0100

    Remove services URL path and add debug print statements in services view

--- webempresa/services/views.py
+++ webempresa/services/views.py
@@ -3,4 +3,7 @@ from .models import Service
 
 def services(request):
     services = Service.objects.all()
+    print(services)
+    for service in services:
+        print(service.title)
     return render(request, "services/services.html", {'services':services})
