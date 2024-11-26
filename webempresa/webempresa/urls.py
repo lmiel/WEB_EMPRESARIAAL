23d8d46015ea080e2d5c20c7ff200a97729659e3
lmiel <lucia.mielgo@gmail.com>
2024-11-26 12:29:13 +0100

    Add blog and services apps with models, views, templates, and migrations

--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -23,7 +23,9 @@ from django.conf import settings
 
 urlpatterns = [
     path("", include("core.urls")),
+    path('services/', include('services.urls')),
     path("admin/", admin.site.urls),
+    
 ]
 
 
