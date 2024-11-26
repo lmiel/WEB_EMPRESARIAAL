0fbb2206342b81a7b5e3385a1484a19ad17bde5e
lmiel <lucia.mielgo@gmail.com>
2024-11-27 00:51:48 +0100

    Add blog app with URL routing, models, views, and templates

--- webempresa/blog/views.py
+++ webempresa/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post, Category
 
-# Create your views here.
+def blog(request):
+    posts = Post.objects.all()
+    
+    for post in posts:
+        print(post)
+    return render(request, "blog/blog.html", {'posts':posts})
--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("", include("core.urls")),
+    path('blog/', include('blog.urls')),
     path('services/', include('services.urls')),
     path("admin/", admin.site.urls),
     
